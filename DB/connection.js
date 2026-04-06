4fae304300907a982aa7565fdbfcab92c1d58c5a
ziadAlattar <ziadalattar22@gmail.com>
2026-04-06 21:18:44 +0200

    dbUrl

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connection = async ()=>{
-    return await mongoose.connect("mongodb+srv://ZiadAlmorsy:00241300@cluster0.mjwgrkh.mongodb.net/chatdb")
+    return await mongoose.connect("mongodb+srv://ZiadAlmorsy:00241300@cluster0.mjwgrkh.mongodb.net/BeEcommerce")
     .then(()=> console.log(`connected on ...... `))
     .catch(err=>console.log(`fail to connect `))
 }
